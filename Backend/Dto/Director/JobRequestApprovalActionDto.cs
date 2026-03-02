d56003c5aad3f589f70c459abd70cc99d6e986f3
Son Huynh <sonhuynh081104@gmail.com>
2026-03-02 23:56:40 +0700

    fix: remove duplicate Director DTO files

--- Backend/Dto/Director/JobRequestApprovalActionDto.cs
+++
@@ -1,8 +0,0 @@
-namespace RMS.Dto.Director;
-
-public class JobRequestApprovalActionDto
-{
-    public int JobRequestId { get; set; }
-    public string? Action { get; set; } // APPROVED, REJECTED, RETURNED
-    public string? Comment { get; set; }
-}
